48f22363415d8854d7aa6b5f1278caba1c01ef55
fafadogoe <92341279+fafadogoe@users.noreply.github.com>
2023-09-03 01:52:32 +0000

    Add files via upload

---
+++ Week 6 Assignment.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarised. 
+
+# __Objectives:__
+# - List some of the commonly used libraries in Data Science
+# - List some Development Environments used by Data Scientists
+# - Convert minutes to hours
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Scikit Learn
+# 2. matplotlib
+# 3. ggplot
+# 4. pandas
+
+# | Data Science Tools |
+# | ------------------ |
+# | Rstudio |
+# | Jupyter Notebook |
+# | Pycharm |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[5]:
+
+
+#this is a simple arithmetic expression to multiply then add integers
+(3*4)+5
+
+
+# In[6]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+200/
+
+
+# ## Author
+# Percy Fafa Kofi Dogoe
